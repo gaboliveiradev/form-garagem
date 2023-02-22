8135136d61759686da996ba2a970d27311d8d755
gaboliveiradev <g38789138@gmail.com>
2023-02-22 08:46:02 -0300

    criando a classe helper email + propriedades e metodo construtor + sendEmail + prepareteMessage

---
+++ Helpers/Email.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormGaragem.Helpers
+{
+    public class Email
+    {
+        public string provedor { get; private set; }
+        public string remetente { get; private set; }
+        public string password { get; private set; }
+
+        public Email(string provedor, string remetente, string password)
+        {
+            this.provedor = provedor ?? throw new ArgumentNullException(nameof(provedor));
+            this.remetente = remetente ?? throw new ArgumentNullException(nameof(remetente));
+            this.password = password ?? throw new ArgumentNullException(nameof(password));
+        }
+
+        public void sendEmail(string emailTo, string subject, string body)
+        {
+
+        }
+
+        private MailMessage prepareteMessage(string emailTo, string subject, string body)
+        {
+            var mail = new MailMessage();
+
+            mail.From = new MailAddress(remetente);
+        }
+    }
+}
