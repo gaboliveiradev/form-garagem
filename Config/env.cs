686b41ed3d4c9aec7d4dc3b84e0800845a841b68
gaboliveiradev <g38789138@gmail.com>
2023-03-07 15:32:19 -0300

    criando as classe closeConnection e openConnection

--- Config/env.cs
+++ Config/env.cs
@@ -2,10 +2,17 @@
 {
     public class env
     {
+        // Configuração de Email SMTP
         public string email = "mocadaautomoveis@hotmail.com";
         public string senha = "etecjau070";
 
         public string servidor_smtp = "smtp.office365.com";
         public int porta_smtp = 587;
+
+        // Configuração do Banco de Dados MySql
+        public string server = "localhost";
+        public string porta = "3307";
+        public string uid = "root";
+        public string pwd = "etecjau";
     }
 }
---
+++ Helpers/MySQL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using FormGaragem.Config;
+using Renci.SshNet.Messages;
+using System.Windows.Forms;
+
+namespace FormGaragem.Helpers
+{
+    public class MySQL
+    {
+        public static MySqlConnection connection;
+        public static MySqlCommand cmd;
+        public static MySqlDataAdapter adapter;
+        public static DataTable dt;
+
+        public static env config;
+
+        public static void openConnection()
+        {
+            try
+            {
+                connection = new MySqlConnection($"server={config.server};port={config.porta};uid={config.uid};pwd={config.pwd}");
+                connection.Open();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "[001] - Erro na classe \"MySQL.cs\" | Metodo \"openConnection();\"", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void closeConnection()
+        {
+            try
+            {
+                connection.Close();
+            } catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "[002] - Erro na classe \"MySQL.cs\" | Metodo \"closeConnection();\"", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
